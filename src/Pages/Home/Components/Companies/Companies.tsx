d0eb1ed454bb5e4df7498289dff03e1a4ece64de
Amal C P <acp070146@gmail.com>
2024-02-04 22:40:06 +0530

    companies section added

---
+++ src/Pages/Home/Components/Companies/Companies.tsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+
+import styles from "./Companies.module.css";
+import Marquee from "react-fast-marquee";
+
+type Partner = {
+  image: string;
+};
+
+type OurPartnersProps = {
+  partners: Partner[];
+};
+
+export const Companies = ({ partners }: OurPartnersProps) => {
+  const [isScreenSmall, setIsScreenSmall] = useState(window.innerWidth < 2020);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsScreenSmall(window.innerWidth < 2000);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const marqParams = {
+    autoFill: true,
+    pauseOnHover: true,
+    gradient: false,
+    speed: 20,
+    drag: true,
+  };
+  return (
+    <div className={styles.Partners}>
+      <div className={styles.PartnersImageWrapper}>
+        {isScreenSmall ? (
+          <Marquee {...marqParams} style={{ width: "100vw" }}>
+            {partners.map((partner, index) => (
+              <div
+                key={index}
+                style={{ padding: "0px 20px", textAlign: "center" }}
+              >
+                <img src={partner.image} alt="companies" />
+              </div>
+            ))}
+          </Marquee>
+        ) : (
+          // Render the images without marquee for larger screens
+          <div style={{ display: "flex" }}>
+            {partners.map((partner, index) => (
+              <div key={index} style={{ padding: "0px 10px" }}>
+                <img src={partner.image} alt="companies" />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
